f73ac3867aaf513f90e299322a8324bef3c85ba8
Mathis <rugby2116@gmail.com>
2023-03-24 17:37:17 +0100

    refont tag

--- scripts/utils/tags.js
+++ scripts/utils/tags.js
@@ -5,178 +5,73 @@ function Tags(resultas) {
   const ustclic = document.querySelectorAll(".casparcasustensils");
 
   let valeur = []; // tableau pour stocker les valeurs des tags
-  
-  
-  // APPAREILS  ----------------------------------------------------------------------------------
-  appclic.forEach((element) => {
-    element.addEventListener("click", appareilsclic);
-    element.addEventListener("click", filtrer);
 
-    function filtrer() {
-      resulta.innerHTML = "";
+  // Fonction pour filtrer les recettes en fonction des tags sélectionnés
 
-      const valeurTag = element.innerText;
-      const objet = resultas;
-      valeur.push(valeurTag); // stocker la valeur du tag
+  function filtrer() {
+    resulta.innerHTML = "";
 
-      const trie = objet.filter((el) =>
-        el.appliance.toLowerCase().includes(valeurTag.toLowerCase())
-      );
-      creaCarte(trie);
-    }
+    const tags = valeur.map((tag) => tag.toLowerCase());
 
-    function appareilsclic() {
-      const tagapp = document.createElement("div");
-      tagapp.setAttribute("class", "tagapp");
-      tagapp.innerHTML = `<div class="flexapp">
-                       <p class="tagtext">${element.innerHTML}</p>
-                       <span class="croix">x</span>
-                       </div>`;
-      filtreTag.appendChild(tagapp);
-
-      const tagRecup = tagapp.querySelector("span");
-      tagRecup.addEventListener("click", close);
-
-      function close() {
-        const contenantTag = tagapp.querySelector("div");
-        contenantTag.style.display = "none";
-
-        // Supprimer la valeur du tag du tableau "valeur"
-        const valeurIndex = valeur.indexOf(element.innerText);
-        if (valeurIndex !== -1) {
-          valeur.splice(valeurIndex, 1);
-        }
-
-        // Filtrer les résultats avec les tags restants
-        resulta.innerHTML = "";
-        const objet = resultas;
-        const trie = objet.filter((el) => {
-          return valeur.every((tag) =>
-            el.appliance.toLowerCase().includes(tag.toLowerCase())
-          );
-        });
-        creaCarte(trie);
-      }
-    }
-  });
-
-  // USTENSILES ----------------------------------------------------------------------------------
-  ustclic.forEach((element) => {
-    element.addEventListener("click", ustensilsappareilsclic);
-    element.addEventListener("click", filtrer);
-
-    function filtrer() {
-      resulta.innerHTML = "";
-
-      const valeurTag = element.innerText;
-      const objet = resultas;
-      valeur.push(valeurTag); // stocker la valeur du tag
-
-      const trie = objet.filter((el) =>
-        el.ustensils.some((ustensil) =>
-          ustensil.toLowerCase().includes(valeurTag.toLowerCase())
-        )
-      );
-      creaCarte(trie);
-    }
-
-    function ustensilsappareilsclic() {
-      const tagust = document.createElement("div");
-      tagust.setAttribute("class", "tagust");
-      tagust.innerHTML = `<div class="flexust">
-                       <p class="tagtext">${element.innerHTML}</p>
-                       <span class="croix">x</span>
-                       </div>`;
-      filtreTag.appendChild(tagust);
-
-      const tagRecup = tagust.querySelector("span");
-      tagRecup.addEventListener("click", close);
-
-      function close() {
-        const contenantTag = tagust.querySelector("div");
-        contenantTag.style.display = "none";
-
-        // Supprimer la valeur du tag du tableau "valeur"
-        const valeurIndex = valeur.indexOf(element.innerText);
-        if (valeurIndex !== -1) {
-          valeur.splice(valeurIndex, 1);
-        }
-
-        const tagsIngredient = Array.from(ingrclic)
-          .filter((element) => valeur.includes(element.innerText))
-          .map((element) => element.innerText);
-
-        const tagsUstensils = Array.from(ustclic)
-          .filter((element) => valeur.includes(element.innerText))
-          .map((element) => element.innerText);
-
-        const trie = resultas.filter(
-          (el) =>
-            tagsIngredient.every((tag) =>
-              el.ingredients.some((ingr) =>
-                ingr.ingredient.toLowerCase().includes(tag.toLowerCase())
-              )
-            ) &&
-            tagsUstensils.every((tag) =>
-              el.ustensils.some((ust) =>
-                ust.toLowerCase().includes(tag.toLowerCase())
-              )
-            )
+    const trie = resultas.filter((el) =>
+      tags.every((tag) => {
+        const tagInIngredients = el.ingredients.some((ingr) =>
+          ingr.ingredient.toLowerCase().includes(tag)
         );
-        creaCarte(trie);
-      }
-    }
-  });
+        const tagInUstensils = el.ustensils.some((ust) =>
+          ust.toLowerCase().includes(tag)
+        );
 
-  // INGREDIENT ----------------------------------------------------------------------------------
-  ingrclic.forEach((element) => {
-    element.addEventListener("click", ingredientclic);
-    element.addEventListener("click", filtrer);
+        const tagInAppareil = el.appliance
+          .toLowerCase()
+          .includes(tag);
+        return tagInIngredients || tagInUstensils || tagInAppareil;
+      })
+    );
+    creaCarte(trie);
+  }
 
-    function filtrer() {
-     resulta.innerHTML = "";
+  // Ajoute un tag au tableau et filtre les recettes
+  const addTag = (element, tagClass) => {
+    element.addEventListener("click", () => {
+      valeur.push(element.innerText);
 
-     const valeurTag = element.innerText;
-     const objet = resultas;
-     valeur.push(valeurTag); // stocker la valeur du tag
-
-     
-    }
-
-    function ingredientclic() {
       const tag = document.createElement("div");
-      tag.setAttribute("class", "tagingr");
-      tag.innerHTML = `<div class="flexingr">
-                      <p class="tagtext"  id="${element.innerText}">${element.innerText}</p>
-                       <span class="croix">x</span>
-                       </div>`;
+      tag.setAttribute("class", tagClass);
+      tag.innerHTML = `<div class="flex${tagClass}">
+                         <p class="tagtext">${element.innerHTML}</p>
+                         <span class="croix">x</span>
+                         </div>`;
+
       filtreTag.appendChild(tag);
 
       const tagRecup = tag.querySelector("span");
-      tagRecup.addEventListener("click", close);
-      function close() {
+      tagRecup.addEventListener("click", () => {
         const contenantTag = tag.querySelector("div");
-        contenantTag.style.display = "none";
-        resulta.innerHTML = "";
+        tag.style.display = "none";
 
-        // Supprimer la valeur du tag sélectionné de la variable 'valeur'
-        const tagValue = element.innerText;
-        const index = valeur.indexOf(tagValue);
-        if (index > -1) {
-          valeur.splice(index, 1);
+        // Supprimer la valeur du tag du tableau "valeur"
+        const valeurIndex = valeur.indexOf(element.innerText);
+        if (valeurIndex !== -1) {
+          valeur.splice(valeurIndex, 1);
         }
+        filtrer();
+      });
+      filtrer();
+    });
+  };
 
-        // Filtrer les résultats en fonction des tags restants
-        const objet = resultas;
-        const trie = objet.filter((el) =>
-          valeur.every((tag) =>
-            el.ingredients.some((ingredient) =>
-              ingredient.ingredient.includes(tag)
-            )
-          )
-        );
-        creaCarte(trie);
-      }
-    }
+  ingrclic.forEach((element) => {
+    addTag(element, "tagingr");
   });
+
+  appclic.forEach((element) => {
+    addTag(element, "tagapp");
+  });
+
+  ustclic.forEach((element) => {
+    addTag(element, "tagust");
+  });
+
+  filtrer();
 }
