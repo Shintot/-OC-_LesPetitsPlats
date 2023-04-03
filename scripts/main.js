91a8189731359c113e9b4e3fef2145e97fb5479c
Mathis <rugby2116@gmail.com>
2023-04-03 19:20:54 +0200

    for ... of

--- scripts/main.js
+++ scripts/main.js
@@ -6,29 +6,42 @@ fetch("recipes.json")
     //LE JSON
     const resultas = data.recipes;
 
-    // TRIE DE RECHERCHE
-     rechercheBar.addEventListener("input", filtre);
-     function filtre(e) {
-     //VIDE LA LISTE
-     resulta.innerHTML = "";
-    const rechercheString = e.target.value.toLowerCase().replace(/\s/g, "");
-
-   const trie = resultas.filter(
-        (el) =>
-        el.name.toLowerCase().replace(/\s/g, "").includes(rechercheString) ||
-          el.appliance.toLowerCase().includes(rechercheString) ||
-         el.ingredients.some((ingredient) =>
-          ingredient.ingredient
+        rechercheBar.addEventListener("input", filtre);
+        function filtre(e) {
+          //VIDE LA LISTE
+          resulta.innerHTML = "";
+          const rechercheString = e.target.value
             .toLowerCase()
-             .replace(/\s/g, "")
-            .includes(rechercheString)
-      ) ||
-       el.ustensils.some((ustensil) =>
-            ustensil.toLowerCase().replace(/\s/g, "").includes(rechercheString)
-        )
-    );
-     creaCarte(trie);
-    }
+            .replace(/\s/g, "");
+
+          const trie = [];
+
+          for (const el of resultas) {
+            if (
+              el.name
+                .toLowerCase()
+                .replace(/\s/g, "")
+                .includes(rechercheString) ||
+              el.appliance.toLowerCase().includes(rechercheString) ||
+              el.ingredients.some((ingredient) =>
+                ingredient.ingredient
+                  .toLowerCase()
+                  .replace(/\s/g, "")
+                  .includes(rechercheString)
+              ) ||
+              el.ustensils.some((ustensil) =>
+                ustensil
+                  .toLowerCase()
+                  .replace(/\s/g, "")
+                  .includes(rechercheString)
+              )
+            ) {
+              trie.push(el);
+            }
+          }
+
+          creaCarte(trie);
+        }
 
     // AFFICHE CARTES
     creaCarte(resultas);
