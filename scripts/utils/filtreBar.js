dd15450ad71fc5e7d67412efd8945470377a82fa
Mathis <rugby2116@gmail.com>
2023-04-03 19:07:43 +0200

    fixe

--- scripts/utils/filtreBar.js
+++ scripts/utils/filtreBar.js
@@ -1,122 +1,60 @@
 //********************** INGREDIENTS *********************************
 
-  pagefiltreIngredient.setAttribute("class", "ingredient__results__displayed");
-  pagefiltreIngredient.style.display = "none";
+pagefiltreIngredient.setAttribute("class", "ingredient__results__displayed");
+pagefiltreIngredient.style.display = "none";
 
-  //BTN UP CACHE
+//BTN UP CACHE
+const btnup = document.querySelector("#boutonup");
+btnup.style.display = "none";
+
+//EVENEMENT OUVERTURE DU FILTRE INGREDIENTS
+document
+  .querySelector(".ingredient__button")
+  .addEventListener("click", ingredientOuvre);
+
+function ingredientOuvre() {
+  var boutonouvert = document.querySelector(".ingredient__results__displayed");
+  boutonouvert.style.display = "grid";
+
+  const btnup = document.querySelector("#boutonup");
+  btnup.style.display = "block";
+
+  const btndown = document.querySelector("#boutondown");
+  btndown.style.display = "none";
+}
+
+//EVENEMENT FERMER
+document.querySelector("#boutonup").addEventListener("click", fermeBtn);
+document.querySelector("#boutonAppareil").addEventListener("click", fermeBtn);
+document
+  .querySelector("#boutonUstensilsDown")
+  .addEventListener("click", fermeBtn);
+
+function fermeBtn() {
+  const boutonouvert = document.querySelector("#ingredient__results");
+  boutonouvert.style.display = "none";
+
+  const btndown = document.querySelector("#boutondown");
+  btndown.style.display = "block";
+
   const btnup = document.querySelector("#boutonup");
   btnup.style.display = "none";
+}
 
-  //EVENEMENT OUVERTURE DU FILTRE INGREDIENTS
-  document
-    .querySelector(".ingredient__button")
-    .addEventListener("click", ingredientOuvre);
-
-  function ingredientOuvre() {
-    var boutonouvert = document.querySelector(".ingredient__results__displayed");
-    boutonouvert.style.display = "grid";
-
-    const btnup = document.querySelector("#boutonup");
-    btnup.style.display = "block";
-
-    const btndown = document.querySelector("#boutondown");
-    btndown.style.display = "none";
-  }
-
-  //EVENEMENT FERMER
-  document.querySelector("#boutonup").addEventListener("click", fermeBtn);
-  document.querySelector("#boutonAppareil").addEventListener("click", fermeBtn);
-  document
-    .querySelector("#boutonUstensilsDown")
-    .addEventListener("click", fermeBtn);
-
-  function fermeBtn() {
-    const boutonouvert = document.querySelector("#ingredient__results");
-    boutonouvert.style.display = "none";
-
-    const btndown = document.querySelector("#boutondown");
-    btndown.style.display = "block";
-
-    const btnup = document.querySelector("#boutonup");
-    btnup.style.display = "none";
-  }
-
-  // APARITION DES INGREDIENTS BOUCLE FOR
-  function apprareilFiltreIngredient(ing) {
-    // ------ VERSION BOUCLE FOR()
-      /* for (let u of ing) {
-        const { ingredients } = u;
-        for (let ingredientList of ingredients) {
-          const { ingredient } = ingredientList;
-          const liIng = document.createElement("li");
-          liIng.setAttribute("class", "casparcas");
-          liIng.innerHTML = `${ingredient}`;
-          pagefiltreIngredient.appendChild(liIng);
-        }
-      }*/
-
-  // ------ VERSION FOREACH()
-  let tableauxIngr = [];
-
-  ing.forEach((element) => {
-    const { ingredients } = element;
-    if (ingredients && ingredients.length > 0) {
-      ingredients.forEach((element) => {
-        const { ingredient } = element;
-        tableauxIngr.push(ingredient);
-      });
-    } else {
-      tableauxIngr.push(element);
-    }
-  });
-  [...new Set(tableauxIngr)].forEach((element) => {
-    tableauxIngrTrie.push(element);
-    const liIng = document.createElement("a");
-    
-    liIng.setAttribute("class", "casparcas");
-    liIng.innerHTML = `${element}`;
-    pagefiltreIngredient.appendChild(liIng);
-  });
-  }
-
-  let tableauxIngrTrie = [];
-
-  resultasIngr.addEventListener("input", rechercheFiltreIngr);
-  function rechercheFiltreIngr(e) {
-  pagefiltreIngredient.innerHTML = "";
-
-  const rechercheString = e.target.value.toLowerCase().replace(/\s/g, "");
-  let regex = new RegExp(rechercheString);
-
-  const trie = tableauxIngrTrie.filter((el) => {
-    return regex.test(el.replace(/\s/g, "").toLowerCase());
-  });
-  //console.log("filtered", trie);
-  apprareilFiltreIngredient(trie);
-  
-  // AFFICHE TAG AU CLIC 
-  Tags()
-  }
-
-
-
+// APARITION DES INGREDIENTS BOUCLE FOR
 
 // ********************** APPAREILS *********************************
 
+//BTN UP CACHE
+const boutonUp = document.querySelector("#boutonAppareilup");
+boutonUp.style.display = "none";
 
+// OUVRE APPAREIL
+const boutonAppareil = document
+  .querySelector("#boutonAppareil")
+  .addEventListener("click", appareils);
 
-
-
-  //BTN UP CACHE
-  const boutonUp = document.querySelector("#boutonAppareilup");
-  boutonUp.style.display = "none";
-
-  // OUVRE APPAREIL
-  const boutonAppareil = document
-    .querySelector("#boutonAppareil")
-    .addEventListener("click", appareils);
-
-  function appareils() {
+function appareils() {
   const appareil = document.querySelector("#appareil__results");
   appareil.classList.add("apparatus__results", "apparatus__results__displayed");
   appareil.style.display = "grid";
@@ -125,18 +63,18 @@
 
   const btnDown = document.querySelector("#boutonAppareil");
   btnDown.style.display = "none";
-  }
+}
 
-  //EVENEMENT FERMER
-  document
-    .querySelector("#boutonAppareilup")
-    .addEventListener("click", fermeAppareil);
-  document.querySelector("#boutondown").addEventListener("click", fermeAppareil);
-  document
-    .querySelector("#boutonUstensilsDown")
-    .addEventListener("click", fermeAppareil);
+//EVENEMENT FERMER
+document
+  .querySelector("#boutonAppareilup")
+  .addEventListener("click", fermeAppareil);
+document.querySelector("#boutondown").addEventListener("click", fermeAppareil);
+document
+  .querySelector("#boutonUstensilsDown")
+  .addEventListener("click", fermeAppareil);
 
-  function fermeAppareil() {
+function fermeAppareil() {
   const fermeBouton = document.querySelector("#appareil__results");
   fermeBouton.style.display = "none";
 
@@ -144,66 +82,21 @@
   btndown.style.display = "block";
 
   boutonUp.style.display = "none";
-  }
+}
 
-  //  ------- ADD LA LISTE D'APPAREILS AU BOUTON
-  pagefiltreAppareil.setAttribute("class", "apparatus__results__undisplayed");
-  function apprareilFiltreAppareil(filtre) {
-  // ------ VERSION BOUCLE FOR()
-  /*for (let uls of filtre) {
-      const { appliance } = uls;
-      console.log(appliance);
-      const li = document.createElement("li");
-      li.innerHTML = `<p class="">${appliance}</p>`;
-      pagefiltreAppareil.appendChild(li);
-    }*/
+//  ------- ADD LA LISTE D'APPAREILS AU BOUTON
+pagefiltreAppareil.setAttribute("class", "apparatus__results__undisplayed");
 
-  // ------ VERSION FOREACH()
-  let tableauxApp = [];
-  filtre.forEach((element) => {
-    const { appliance } = element;
-    tableauxApp.push(appliance);
-    tableauxAppTrie.push(element);
-  });
-  [...new Set(tableauxApp)].forEach((element) => {
-    const li = document.createElement("a");
-    li.setAttribute("class", "casparcasappareil");
-    li.innerHTML = `<p>${element}</p>`;
-    pagefiltreAppareil.appendChild(li);
-  });
-  }
+// ********************** USTENSILS *********************************
 
-  // ------ RECHERCHE APPAREILS
-  let tableauxAppTrie = [];
+const up = document.querySelector("#boutonUstensils");
+up.style.display = "none";
 
-  resultasApp.addEventListener("input", rechercheFiltreApp);
-  function rechercheFiltreApp(e) {
-    pagefiltreAppareil.innerHTML = "";
-    const rechercheString = e.target.value.toLowerCase().replace(/\s/g, "");
+document
+  .querySelector("#boutonUstensilsDown")
+  .addEventListener("click", ustensils);
 
-    const trie = tableauxAppTrie.filter((el) =>
-      el.appliance.toLowerCase().replace(/\s/g, "").includes(rechercheString)
-    );
-    apprareilFiltreAppareil(trie);
-
-    // AFFICHE TAG AU CLIC
-    Tags();
-  }
-
-
-
-  // ********************** USTENSILS *********************************
-
-
-
-  const up = document.querySelector("#boutonUstensils");
-  up.style.display = "none";
-
-  document
-    .querySelector("#boutonUstensilsDown")
-    .addEventListener("click", ustensils);
-
-  function ustensils() {
+function ustensils() {
   const appareil = document.querySelector("#ustensils__results");
   appareil.classList.add("ustensils__results", "ustensils__results__displayed");
   appareil.style.display = "grid";
@@ -212,13 +105,13 @@
   boutonUstensils.style.display = "none";
 
   up.style.display = "block";
-  }
+}
 
-  document.querySelector("#boutonUstensils").addEventListener("click", ferme);
-  document.querySelector("#boutonAppareil").addEventListener("click", ferme);
-  document.querySelector("#boutondown").addEventListener("click", ferme);
+document.querySelector("#boutonUstensils").addEventListener("click", ferme);
+document.querySelector("#boutonAppareil").addEventListener("click", ferme);
+document.querySelector("#boutondown").addEventListener("click", ferme);
 
-  function ferme() {
+function ferme() {
   var appareil = document.querySelector("#ustensils__results");
   appareil.style.display = "none";
 
@@ -227,68 +120,10 @@
 
   const up = document.querySelector("#boutonUstensils");
   up.style.display = "none";
-  }
+}
 
-  //  ------- ADD LA LISTE D'USTENSILES AU BOUTON
-  pagefiltreUstensiles.setAttribute("class", "apparatus__results__undisplayed");
-  function apprareilFiltreUstensiles(ust) {
-  // ------ VERSION BOUCLE FOR()
-  /*for (let materiel of ust) {
-    const { ustensils } = materiel;
-    for (let ustensil of ustensils) {
-      //console.log(ustensil);
-      const liUst = document.createElement("li");
-      liUst.setAttribute("class", "test");
-      liUst.innerHTML = `<p class="">${ustensil}</p>`;
-      pagefiltreUstensiles.appendChild(liUst);
-    }
-  }*/
+//  ------- ADD LA LISTE D'USTENSILES AU BOUTON
+pagefiltreUstensiles.setAttribute("class", "apparatus__results__undisplayed");
 
-  // ------ VERSION FOREACH()
-  let tableauxUst = [];
-
-  ust.forEach((element) => {
-    const { ustensils } = element;
-
-    if (ustensils && ustensils.length > 0) {
-      ustensils.forEach((element) => {
-        tableauxUst.push(element);
-      });
-    } else {
-      tableauxUst.push(element);
-    }
-  });
-  [...new Set(tableauxUst)].forEach((element) => {
-    tableauxTrie.push(element);
-    // const lien = document.createElement("a");
-    const liUst = document.createElement("a");
-    liUst.setAttribute("class", "casparcasustensils");
-    liUst.innerHTML = `<p class="tagtext">${element}</p>`;
-    pagefiltreUstensiles.appendChild(liUst);
-  });
-  }
-
-  // ------ RECHERCHE USTENSILS
-  let tableauxTrie = [];
-  //console.log(tableauxTrie);
-
-  resultasUst.addEventListener("input", rechercheFiltreUst);
-  function rechercheFiltreUst(e) {
-    pagefiltreUstensiles.innerHTML = "";
-
-    const rechercheString = e.target.value.toLowerCase().replace(/\s/g, "");
-
-    let regex = new RegExp(rechercheString);
-    const trie = tableauxTrie.filter((el) => {
-      console.log(el);
-      return regex.test(el.replace(/\s/g, "").toLowerCase());
-    });
-    //console.log("filtered", trie);
-    apprareilFiltreUstensiles(trie);
-    // AFFICHE TAG AU CLIC
-    Tags();
-  }
-  
-
-  
-
+// ------ RECHERCHE USTENSILS
+let tableauxTrie = [];
--- scripts/utils/tags.js
+++ scripts/utils/tags.js
@@ -1,77 +1,244 @@
-function Tags(resultas) {
-  const filtreTag = document.querySelector(".research__filters");
-  const ingrclic = document.querySelectorAll(".casparcas");
-  const appclic = document.querySelectorAll(".casparcasappareil");
-  const ustclic = document.querySelectorAll(".casparcasustensils");
+let valeur = [];
+let filtreTag = document.querySelector(".research__filters");
+let appclic = document.querySelectorAll(".casparcasappareil");
+let ustclic = document.querySelectorAll(".casparcasustensils");
+let ingrclic = document.querySelectorAll(".casparcas");
 
-  let valeur = []; // tableau pour stocker les valeurs des tags
+function filtrer(resultas) {
+  resulta.innerHTML = "";
+  const tags = valeur.map((tag) => tag.toLowerCase());
 
-  // Fonction pour filtrer les recettes en fonction des tags sélectionnés
+  const trie = resultas.filter((el) => {
+    return tags.every((tag) => {
+      const tagInIngredients = el.ingredients.some((ingr) =>
+        ingr.ingredient.toLowerCase().includes(tag)
+      );
+      const tagInUstensils = el.ustensils.some((ust) =>
+        ust.toLowerCase().includes(tag)
+      );
 
-  function filtrer() {
-    resulta.innerHTML = "";
+      const tagInAppareil = el.appliance.toLowerCase().includes(tag);
+      return tagInIngredients || tagInUstensils || tagInAppareil;
+    });
+  });
 
-    const tags = valeur.map((tag) => tag.toLowerCase());
+  return trie;
+}
 
-    const trie = resultas.filter((el) =>
-      tags.every((tag) => {
-        const tagInIngredients = el.ingredients.some((ingr) =>
-          ingr.ingredient.toLowerCase().includes(tag)
-        );
-        const tagInUstensils = el.ustensils.some((ust) =>
-          ust.toLowerCase().includes(tag)
-        );
-
-        const tagInAppareil = el.appliance
-          .toLowerCase()
-          .includes(tag);
-        return tagInIngredients || tagInUstensils || tagInAppareil;
-      })
-    );
-    creaCarte(trie);
+const addTag = (element, tagClass, resultas) => {
+  if (element.hasAttribute("data-tag-clicked")) {
+    return;
   }
 
-  // Ajoute un tag au tableau et filtre les recettes
-  const addTag = (element, tagClass) => {
-    element.addEventListener("click", () => {
-      valeur.push(element.innerText);
+  element.setAttribute("data-tag-clicked", "true");
 
-      const tag = document.createElement("div");
-      tag.setAttribute("class", tagClass);
-      tag.innerHTML = `<div class="flex${tagClass}">
-                         <p class="tagtext">${element.innerHTML}</p>
-                         <span class="croix">x</span>
-                         </div>`;
+  filteredRecipes = filtrer(resultas);
 
-      filtreTag.appendChild(tag);
+  element.addEventListener("click", () => {
+    valeur.push(element.innerText);
 
-      const tagRecup = tag.querySelector("span");
-      tagRecup.addEventListener("click", () => {
-        const contenantTag = tag.querySelector("div");
-        tag.style.display = "none";
+    const tag = document.createElement("div");
+    tag.setAttribute("class", tagClass);
+    tag.innerHTML = `<div class="flex${tagClass}">
+                       <p class="tagtext">${element.innerHTML}</p>
+                       <span class="croix">x</span>
+                       </div>`;
 
-        // Supprimer la valeur du tag du tableau "valeur"
-        const valeurIndex = valeur.indexOf(element.innerText);
-        if (valeurIndex !== -1) {
-          valeur.splice(valeurIndex, 1);
-        }
-        filtrer();
-      });
-      filtrer();
+    filtreTag.appendChild(tag);
+
+    apprareilFiltreIngredient(filteredRecipes);
+    apprareilFiltreAppareil(filteredRecipes);
+    apprareilFiltreUstensiles(filteredRecipes);
+
+    const tagRecup = tag.querySelector("span");
+    tagRecup.addEventListener("click", () => {
+      const contenantTag = tag.querySelector("div");
+      tag.style.display = "none";
+
+      // Supprimer la valeur du tag du tableau "valeur"
+      const valeurIndex = valeur.indexOf(element.innerText);
+      if (valeurIndex !== -1) {
+        valeur.splice(valeurIndex, 1);
+      }
+      filteredRecipes = filtrer(resultas);
+
+      apprareilFiltreIngredient(filteredRecipes);
+      apprareilFiltreAppareil(filteredRecipes);
+      apprareilFiltreUstensiles(filteredRecipes);
+      creaCarte(filteredRecipes);
     });
-  };
 
+    creaCarte(filteredRecipes);
+  });
+};
+
+function Tags(resultas) {
   ingrclic.forEach((element) => {
-    addTag(element, "tagingr");
+    addTag(element, "tagingr", resultas);
   });
 
   appclic.forEach((element) => {
-    addTag(element, "tagapp");
+    addTag(element, "tagapp", resultas);
   });
 
   ustclic.forEach((element) => {
-    addTag(element, "tagust");
+    addTag(element, "tagust", resultas);
   });
 
-  filtrer();
+  let filteredRecipes = filtrer(resultas);
+  creaCarte(filteredRecipes);
 }
+
+let tableauxIngrTrie = [];
+
+function apprareilFiltreIngredient(ing) {
+  let tableauxIngr = [];
+
+  let filteredRecipe = filtrer(ing);
+
+  filteredRecipe.forEach((recipe) => {
+    recipe.ingredients && recipe.ingredients.length > 0
+      ? recipe.ingredients.forEach((element) =>
+          tableauxIngr.push(element.ingredient)
+        )
+      : tableauxIngr.push([]);
+  });
+
+  tableauxIngrTrie = [...new Set(tableauxIngr)].sort();
+
+  const list = document.querySelector("#ingredient__results");
+
+  list.innerHTML = tableauxIngrTrie
+    .map((element) => `<li class="casparcas">${element}</li>`)
+    .join("");
+
+  ingrclic = document.querySelectorAll(".casparcas");
+
+  Tags(filteredRecipe);
+}
+
+const inputIngredient = document.querySelector("#chercheIngr");
+inputIngredient.addEventListener("input", function () {
+  const rechercheString = inputIngredient.value
+    .toLowerCase()
+    .replace(/\s/g, "");
+  const regex = new RegExp(rechercheString);
+
+  const listItems = document.querySelectorAll(".casparcas");
+  const filteredItems = Array.from(listItems).filter((li) =>
+    regex.test(li.textContent.toLowerCase().replace(/\s/g, ""))
+  );
+
+  filteredItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
+
+  const list = document.querySelector("#ingredient__results");
+
+  if (rechercheString === "") {
+    // réinitialiser la liste si la chaîne de recherche est vide
+    list.innerHTML = tableauxIngrTrie
+      .concat()
+      .map((element) => `<li class="casparcas">${element}</li>`)
+      .join("");
+  } else {
+    list.innerHTML = "";
+    filteredItems.forEach((li) => list.appendChild(li));
+  }
+});
+
+let tableauxAppTrie = [];
+
+function apprareilFiltreAppareil(filtre) {
+  let filteredRecipe = filtrer(filtre);
+  tableauxApp = filteredRecipe.map((element) => element.appliance);
+
+  tableauxAppTrie = [...new Set(tableauxApp)].sort();
+
+  const list = document.querySelector("#appareil__results");
+
+  list.innerHTML = tableauxAppTrie
+    .map((element) => `<li class="casparcasappareil">${element}</li>`)
+    .join("");
+
+  appclic = document.querySelectorAll(".casparcasappareil");
+
+  Tags(filteredRecipe);
+}
+
+const inputAppareil = document.querySelector("#chercheApp");
+inputAppareil.addEventListener("input", function () {
+  const rechercheString = inputAppareil.value.toLowerCase().replace(/\s/g, "");
+  const regex = new RegExp(rechercheString);
+
+  const listItems = document.querySelectorAll(".casparcasappareil");
+  const filteredItems = Array.from(listItems).filter((li) =>
+    regex.test(li.textContent.toLowerCase().replace(/\s/g, ""))
+  );
+
+  filteredItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
+
+  const list = document.querySelector("#appareil__results");
+
+  if (rechercheString === "") {
+    // réinitialiser la liste si la chaîne de recherche est vide
+    list.innerHTML = tableauxAppTrie
+      .concat()
+      .map((element) => `<li class="casparcasappareil">${element}</li>`)
+      .join("");
+  } else {
+    list.innerHTML = "";
+    filteredItems.forEach((li) => list.appendChild(li));
+  }
+});
+
+let tableauxUstTrie = [];
+
+function apprareilFiltreUstensiles(ust) {
+  let tableauxUst = [];
+  console.log("base", tableauxUst);
+
+  let filteredRecipe = filtrer(ust);
+
+  filteredRecipe.forEach((recipe) => {
+    recipe.ustensils && recipe.ustensils.length > 0
+      ? tableauxUst.push(...recipe.ustensils)
+      : tableauxUst.push([]);
+  });
+
+  tableauxUstTrie = [...new Set(tableauxUst)].sort();
+  console.log("trie", tableauxUstTrie);
+
+  const list = document.querySelector("#ustensils__results");
+  list.innerHTML = tableauxUstTrie
+    .map((element) => `<li class="casparcasustensils">${element}</li>`)
+    .join("");
+
+  ustclic = document.querySelectorAll(".casparcasustensils");
+
+  Tags(filteredRecipe);
+}
+
+const input = document.querySelector("#rechercheUsteniles");
+input.addEventListener("input", function () {
+  const rechercheString = input.value.toLowerCase().replace(/\s/g, "");
+  const regex = new RegExp(rechercheString);
+
+  const listItems = document.querySelectorAll(".casparcasustensils");
+  const filteredItems = Array.from(listItems).filter((li) =>
+    regex.test(li.textContent.toLowerCase().replace(/\s/g, ""))
+  );
+
+  filteredItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
+
+  const list = document.querySelector("#ustensils__results");
+
+  if (rechercheString === "") {
+    // réinitialiser la liste si la chaîne de recherche est vide
+    list.innerHTML = tableauxUstTrie
+      .concat()
+      .map((element) => `<li class="casparcasustensils">${element}</li>`)
+      .join("");
+  } else {
+    list.innerHTML = "";
+    filteredItems.forEach((li) => list.appendChild(li));
+  }
+});
