d0f6752e1e39cf930353e504769706eea48a8881
Mathis <rugby2116@gmail.com>
2023-04-03 19:14:04 +0200

    method filter

--- scripts/main.js
+++ scripts/main.js
@@ -3,33 +3,31 @@
 fetch("recipes.json")
   .then((response) => response.json())
   .then((data) => {
-    
     //LE JSON
     const resultas = data.recipes;
-   
-    
-    // TRIE DE RECHERCHE 
-    rechercheBar.addEventListener("input", filtre);
-    function filtre(e) {
-      //VIDE LA LISTE
-      resulta.innerHTML = "";
-      const rechercheString = e.target.value.toLowerCase().replace(/\s/g, "");
-       
-      const trie = resultas.filter(
+
+    // TRIE DE RECHERCHE
+     rechercheBar.addEventListener("input", filtre);
+     function filtre(e) {
+     //VIDE LA LISTE
+     resulta.innerHTML = "";
+    const rechercheString = e.target.value.toLowerCase().replace(/\s/g, "");
+
+   const trie = resultas.filter(
         (el) =>
-          el.name.toLowerCase().replace(/\s/g, "").includes(rechercheString) ||
+        el.name.toLowerCase().replace(/\s/g, "").includes(rechercheString) ||
           el.appliance.toLowerCase().includes(rechercheString) ||
-          el.ingredients.some((ingredient) =>
-            ingredient.ingredient
-              .toLowerCase()
-              .replace(/\s/g, "")
-              .includes(rechercheString)
-          ) ||
-          el.ustensils.some((ustensil) =>
+         el.ingredients.some((ingredient) =>
+          ingredient.ingredient
+            .toLowerCase()
+             .replace(/\s/g, "")
+            .includes(rechercheString)
+      ) ||
+       el.ustensils.some((ustensil) =>
             ustensil.toLowerCase().replace(/\s/g, "").includes(rechercheString)
-          )
-      );
-      creaCarte(trie);
+        )
+    );
+     creaCarte(trie);
     }
 
     // AFFICHE CARTES
@@ -39,11 +37,9 @@ fetch("recipes.json")
     apprareilFiltreAppareil(resultas);
     apprareilFiltreIngredient(resultas);
     apprareilFiltreUstensiles(resultas);
-    
+
     // TAGS DE RECHERCHE
     Tags(resultas);
-
-    
   })
  
     //FIN
